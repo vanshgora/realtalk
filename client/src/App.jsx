4413badea1e4028bdccee3f2fe11a54478e95874
vansh <vanshgora31@gmail.com>
2025-02-12 23:41:21 +0530

    created basic frontend for sending messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,77 @@
-import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useRef, useState } from 'react'
 import './App.css'
 import "react-chat-elements/dist/main.css"
 import useWebSocket from 'react-use-websocket'
+import { Avatar, Button, Input, MessageBox, Navbar } from 'react-chat-elements'
 
 function App() {
   const [msg, setMsg] = useState('');
   const [msgList, setMsgList] = useState([]);
+  const inputRef = useRef(null);
   const ws = useWebSocket('ws://localhost:3000', {
     onMessage: (message) => {
-      console.log('Received:', message.data);
       setMsgList([...msgList, message.data.toString()]);
     }
   });
-  // const handleClickSendMessage = useCallback(() => ws.sendMessage(msg), []);
+
+  const clearInput = () => {
+    inputRef.current.value = '';
+  }
+  
+  const handleClickSendMessage = useCallback(() => {
+    ws.sendMessage(msg);
+    setMsg('');
+    clearInput();
+  });
 
   return (
     <>
-      <form>
-        <input type="text" name="chat" placeholder="Write Something" value={msg} onChange={(e) => setMsg(e.target.value)} />
-        <button type="submit" onClick={(e) => {
-          e.preventDefault();
-          ws.sendMessage(msg);
-        }
-        }>Send</button>
-      </form>
-      <div className="messages-div">
-        <ol className="massages">
-          {
-            msgList.map((message, index) => <li className='message' key={index}>{message}</li>)
-          }
-        </ol>
+      <div className="h-screen bg-gray-100">
+        <div className="grid bg-gray-500 grid-rows-[70px_1fr_70px] h-full">
+
+          <Navbar
+            className='chat-window-nav  p-4'
+            left={
+              <Avatar
+                src="https://avatars.githubusercontent.com/u/80540635?v=4"
+                alt="avatar"
+                size="xlarge"
+                type="rounded"
+              />
+            }
+          />
+
+          <main className="messages-div p-4">
+            <ol className="massages h-full">
+              {
+                msgList.map((message, index) => <MessageBox
+                  key={index}
+                  position={"left"}
+                  type={"text"}
+                  title={"Message Box Title"}
+                  text={message}
+                  styles={{ color: 'black' }}
+                  className='my-5'
+                />)
+              }
+            </ol>
+          </main>
+
+          <div className="message-form flex flex-row w-full">
+            <Input
+              placeholder="Type here..."
+              multiline={true}
+              className='message-input'
+              type='text'
+              onChange={(e) => setMsg(e.target.value)}
+              referance={inputRef}
+            />
+            <Button text={"Send"} onClick={handleClickSendMessage} title="Send" />
+          </div>
+
+        </div>
       </div>
+
     </>
   )
 }
