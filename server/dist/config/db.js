8dde14120cf976aaf596e5d89b890bd1c5c76950
vansh <vanshgora31@gmail.com>
2025-02-15 17:12:48 +0530

    pushed server dist to git

---
+++ server/dist/config/db.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    }
+    catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+exports.default = connectDB;
